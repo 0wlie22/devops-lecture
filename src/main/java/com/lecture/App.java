40076a185516a85b12b7a4eb037668e1cb280845
Darja Sedova <dari.sedova@gmail.com>
2026-08-28 14:04:51 +0300

    add new text sentence

--- src/main/java/com/lecture/App.java
+++ src/main/java/com/lecture/App.java
@@ -2,10 +2,11 @@ package com.lecture;
 
 public class App {
     public static String message() {
-        return "Hello, welcome to DevOps lecture!";
+        return "Hello, welcome to AI lecture";
     }
 
     public static void main(String[] args) {
         System.out.println(message());
+        System.out.println("Hello, one more time!");
     }
 }
